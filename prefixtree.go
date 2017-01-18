2fa1bd4081e823c695a5f3fe8f1be9cbdd0ae0cd
Vee Satayamas <vsatayamas@gmail.com>
2017-01-18 14:37:45 +0700

    add prefix tree

---
+++ prefixtree.go
@@ -0,0 +1,83 @@
+package mapkha
+
+import "sort"
+
+// WordWithPayload is a pair of word and its payload
+type WordWithPayload struct {
+	Word    string
+	Payload interface{}
+}
+
+// PrefixTreeNode represents node in a prefix tree
+type PrefixTreeNode struct {
+	NodeID int
+	Offset int
+	Ch     rune
+}
+
+// PrefixTreePointer is partial information of edge
+type PrefixTreePointer struct {
+	ChildID int
+	IsFinal bool
+	Payload interface{}
+}
+
+// PrefixTree is a Hash-based Prefix Tree for searching words
+type PrefixTree struct {
+	tab map[PrefixTreeNode]PrefixTreePointer
+}
+
+type byWord []WordWithPayload
+
+func (wordsWithPayload byWord) Len() int {
+	return len(wordsWithPayload)
+}
+
+func (wordsWithPayload byWord) Swap(i, j int) {
+	wordsWithPayload[i], wordsWithPayload[j] =
+		wordsWithPayload[j], wordsWithPayload[i]
+}
+
+func (wordsWithPayload byWord) Less(i, j int) bool {
+	return wordsWithPayload[i].Word < wordsWithPayload[j].Word
+}
+
+// MakePrefixTree is for constructing prefix tree for word with payload list
+func MakePrefixTree(wordsWithPayload []WordWithPayload) *PrefixTree {
+	sort.Sort(byWord(wordsWithPayload))
+	tab := make(map[PrefixTreeNode]PrefixTreePointer)
+
+	for i, wordWithPayload := range wordsWithPayload {
+		word := wordWithPayload.Word
+		payload := wordWithPayload.Payload
+		rowNo := 0
+
+		runes := []rune(word)
+		for j, ch := range runes {
+			isFinal := ((j + 1) == len(runes))
+			node := PrefixTreeNode{rowNo, j, ch}
+			child, found := tab[node]
+
+			if !found {
+				var thisPayload interface{}
+				if isFinal {
+					thisPayload = payload
+				} else {
+					thisPayload = nil
+				}
+				pointer := PrefixTreePointer{i, isFinal, thisPayload}
+				tab[node] = pointer
+				rowNo = i
+			} else {
+				rowNo = child.ChildID
+			}
+		}
+	}
+	return &PrefixTree{tab}
+}
+
+// Lookup - look up prefix tree from node-id, offset and a character
+func (tree *PrefixTree) Lookup(nodeID int, offset int, ch rune) (*PrefixTreePointer, bool) {
+	pointer, found := tree.tab[PrefixTreeNode{nodeID, offset, ch}]
+	return &pointer, found
+}
---
+++ prefixtree_test.go
@@ -0,0 +1,107 @@
+package mapkha
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestOneCharPrefixTree(t *testing.T) {
+	words := [...]WordWithPayload{WordWithPayload{"A", 10}}
+	prefixTree := MakePrefixTree(words[:])
+	expect := &PrefixTreePointer{0, true, 10}
+	child, found := prefixTree.Lookup(0, 0, 'A')
+
+	if !found {
+		t.Errorf("Expect to find 0, 0, A")
+	}
+
+	if !reflect.DeepEqual(expect, child) {
+		t.Errorf("Expect %q got %q", expect, child)
+	}
+}
+
+func TestOneWordPrefixTree(t *testing.T) {
+	words := [...]WordWithPayload{WordWithPayload{"AB", 20}}
+	prefixTree := MakePrefixTree(words[:])
+
+	var expect *PrefixTreePointer
+	var child *PrefixTreePointer
+	var found bool
+
+	expect = &PrefixTreePointer{0, false, nil}
+	child, found = prefixTree.Lookup(0, 0, 'A')
+
+	if !found {
+		t.Errorf("Expect to find 0, 0, A")
+	}
+
+	if !reflect.DeepEqual(expect, child) {
+		t.Errorf("Expect %q got %q", expect, child)
+	}
+
+	expect = &PrefixTreePointer{0, true, 20}
+	child, found = prefixTree.Lookup(0, 1, 'B')
+
+	if !found {
+		t.Errorf("Expect to find 0, 1, B")
+	}
+
+	if !reflect.DeepEqual(expect, child) {
+		t.Errorf("Expect %q got %q", expect, child)
+	}
+}
+
+func TestTwoWordsPrefixTree(t *testing.T) {
+	words := [...]WordWithPayload{WordWithPayload{"AB", 20},
+		WordWithPayload{"AC", 30}, WordWithPayload{"D", 40}}
+	prefixTree := MakePrefixTree(words[:])
+
+	var expect *PrefixTreePointer
+	var child *PrefixTreePointer
+	var found bool
+
+	expect = &PrefixTreePointer{0, false, nil}
+	child, found = prefixTree.Lookup(0, 0, 'A')
+
+	if !found {
+		t.Errorf("Expect to find 0, 0, A")
+	}
+
+	if !reflect.DeepEqual(expect, child) {
+		t.Errorf("Expect %q got %q", expect, child)
+	}
+
+	expect = &PrefixTreePointer{0, true, 20}
+	child, found = prefixTree.Lookup(0, 1, 'B')
+
+	if !found {
+		t.Errorf("Expect to find 0, 1, B")
+	}
+
+	if !reflect.DeepEqual(expect, child) {
+		t.Errorf("Expect %q got %q", expect, child)
+	}
+
+	expect = &PrefixTreePointer{1, true, 30}
+	child, found = prefixTree.Lookup(0, 1, 'C')
+
+	if !found {
+		t.Errorf("Expect to find 0, 1, C")
+	}
+
+	if !reflect.DeepEqual(expect, child) {
+		t.Errorf("Expect %q got %q", expect, child)
+	}
+
+	expect = &PrefixTreePointer{2, true, 40}
+	child, found = prefixTree.Lookup(0, 0, 'D')
+
+	if !found {
+		t.Errorf("Expect to find 0, 0, D")
+	}
+
+	if !reflect.DeepEqual(expect, child) {
+		t.Errorf("Expect %q got %q", expect, child)
+	}
+
+}
